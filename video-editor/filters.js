e81b74c3eb51df503239f49408da7d057b9a8f20
Derek Enos <derek.enos@gmail.com>
2020-05-21 20:48:28 -0400

    dded fullscreen button to final canvas and hid video element.

--- video-editor/filters.js
+++ video-editor/filters.js
@@ -2,15 +2,20 @@
 function* pixelIterator (imageData) {
   const maxI = imageData.width * imageData.height * 4
   const data = imageData.data
+  let row = 0
+  let col = 0
   for (let i = 0; i < maxI; i += 4) {
-    yield [ i, data[i], data[i + 1], data[i + 2], data[i + 3] ]
+    yield [ i, col, row, data[i], data[i + 1], data[i + 2], data[i + 3] ]
+    col += 1
+    if (col === imageData.width) {
+      row += 1
+      col = 0
+    }
   }
 }
 
 export function applyPixelFilters(filters, imageData) {
-  let row = 0
-  let col = 0
-  for (let [ i, ...pixel ] of pixelIterator(imageData)) {
+  for (let [ i, col, row, ...pixel ] of pixelIterator(imageData)) {
     for (let filter of filters) {
       pixel = filter(pixel, i, col, row)
     }
@@ -18,11 +23,6 @@ export function applyPixelFilters(filters, imageData) {
     imageData.data[i + 1] = pixel[1]
     imageData.data[i + 2] = pixel[2]
     imageData.data[i + 3] = pixel[3]
-    col += 1
-    if (col === imageData.width) {
-      row += 1
-      col = 0
-    }
   }
   return imageData
 }
